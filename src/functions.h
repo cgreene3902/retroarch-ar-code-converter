cd6fb3a42a2a598da59b805a1cdcf86e091eed91
cgreene3902 <cgreene39022@protonmail.com>
2022-09-03 16:27:17 -0400

    very small change to functions.h and .gitignore

--- src/functions.h
+++ src/functions.h
@@ -4,7 +4,7 @@
 char * UserInput();
 // Checks the string to make sure it is a valid input, assumes the input is invaild by default.
 // 
-// The string should contain at least 1 plus sign to be vaild and must occur between a set of 8 ASCII characters,
+// The string should contain at least 1 plus sign to be vaild and must occur between a set of 8 ASCII characters
 // like this 00000000+00000000.
 int stringcheck(char *_input);
 void printing(char *_input);
